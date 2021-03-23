a6e8f3c6795bfef055636fd5695179bcc8d3a7c5
taiseen <taiseen.cse@gmail.com>
2021-03-23 18:24:12 +0600

    Learn & Create API's

--- index.js
+++ index.js
@@ -3,10 +3,75 @@
 var express = require('express')
 var app = express()
 
-const rootCall = (req , res) => res.send('Thank You So Much & Port Number : 3000') ;
+
+//##########################################
+//##########################################
+//##########################################
+// User API
+const rootCall = (req, res) => {
+    const info = '<h1>Welcome To Node Js</h1>'
+    res.send(info);
+};
 app.get('/', rootCall)
 
-const listening = () => console.log('Listening at port : 3000');
+// user get this info by going to this url
+// http://localhost:3000
+
+
+
+
+//##########################################
+//##########################################
+//##########################################
+// User API
+const userInfo = (req, res) => {
+    const info = {
+        name: 'taiseen',
+        age: 29,
+        gender: 'Male',
+        home: 'Dhaka'
+    }
+    res.send(info);
+    // you will return this object, 
+    // when anyone calls the /user API
+};
+app.get('/user', userInfo)
+
+
+// user get this info by going to this url
+// http://localhost:3000/user
+
+
+
+
+//##########################################
+//##########################################
+//##########################################
+// Fruit API
+const banana = (req, res) => {
+    const info = {
+        name: 'banana',
+        price: 10,
+        quantity: 300,
+        totalPrice: 3000,
+    }
+    res.send(info);
+};
+app.get('/fruit', banana);
+
+// user get this info by going to this url
+// http://localhost:3000/fruit/
+
+
+
+
+//##########################################
+//##########################################
+//##########################################
+
+const listening = () => {
+    console.log('Listening at port : 3000')
+}
 app.listen(3000, listening)
 
 
